089eab1a8f761a66622362f5c6c639ddd26ffc5d
HeckerCreatives <113580254+HeckerCreatives@users.noreply.github.com>
2024-07-19 14:43:54 +0800

    pooosh

--- Assets/002 - BattleRoyaleMultiplayer/000 - Script/000 - Core/DedicatedServerManager.cs
+++ Assets/002 - BattleRoyaleMultiplayer/000 - Script/000 - Core/DedicatedServerManager.cs
@@ -453,7 +453,7 @@ public class DedicatedServerManager : NetworkBehaviour, IPlayerJoined, IPlayerLe
 
             if (WaitingAreaTimer <= 0f)
             {
-                WaitingAreaTimer = 20f;
+                WaitingAreaTimer = 120f;
                 CanCountWaitingAreaTimer = true;
             }
 
